defe71db7ffb4a12cfb991807722d8dbf18c6881
C4rim-cibersegurata <mendezluciano1999@gmail.com>
2022-11-08 13:19:24 +0100

    models commit

---
+++ MVC3/src/main/java/models/Proyectos.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Proyectos {
+	// M�todo para crear la tabla
+	public void createTable() {
+		Connection c = ConexionSQL.connection;
+		try {
+			// Borrar la tabla en caso que exista
+			String query = "DROP TABLE IF EXISTS proyecto";
+			Statement st = c.createStatement();
+			st.executeUpdate(query);
+			// Crear la tabla y su estructura
+			query = "CREATE TABLE proyecto(" 
+					+ "id CHAR(4) PRIMARY KEY, "
+					+ "nombre NVARCHAR(255), "
+					+ "horas INT "
+					+ ");";
+			st.executeUpdate(query);
+			System.out.println("Tabla creada con éxito!");
+
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println("Error creando la tabla.");
+		}
+	}
+
+	// M�todo para insertar datos en la tabla
+	public void insertCientificos() {
+		Connection c = ConexionSQL.connection;
+		try {
+			String query = "INSERT INTO proyecto (id, nombre, horas) values"
+					+ "('12', 'Manhattan', 11304),"
+					+ "('6', 'Dolly', 42);";
+
+			Statement st = c.createStatement();
+			st.executeUpdate(query);
+			System.out.println("Datos insertados con exito!");
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println("Error al insertar datos.");
+		}
+	}
+}
